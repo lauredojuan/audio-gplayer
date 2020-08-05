b80a5f653336970c9f1ae362b5364ac7102a9dec
Juan L <lauredojuan@gmail.com>
2020-08-05 21:02:48 +0000

    updated

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export class Home extends React.Component {
 	constructor() {
 		super();
+		//create a reference to the audio player
+		// this.audio = React.createRef();
+		this.audio = null; //will not change on reload. prefered: more flexible than method above
+
 		this.state = {
+			//changes on reload
 			currentIndex: 1,
 			songs: [
 				{
@@ -35,29 +37,77 @@ export class Home extends React.Component {
 		};
 	}
 
-	async change(i) {
-		await this.setState({ currentIndex: i });
+	componentDidMount() {
+		this.pauseButton.style.display = "none";
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(response => response.json())
+			.then(songs => this.setState({ songs }));
 	}
+	//Every time you click on and <li></li> it will use the reference to stop the music player and reload it with a new song
+	changeTrack(i) {
+		this.setState({ currentIndex: i });
+		this.audio.current.pause();
+		this.audio.current.load();
+		this.audio.current.play();
+	}
+	play = i => {
+		var url = this.state.songs[i].url;
+		const songUrl = "https://assets.breatheco.de/apis/sound/" + url;
+		this.audio.src = songUrl;
+		this.audio.play();
+		this.playButton.style.display = "none";
+		this.pauseButton.style.display = "inline-block";
+		this.setState({ currentIndex: i });
+	};
+	pause = () => {
+		this.audio.pause();
+		this.playButton.style.display = "inline-block";
+		this.pauseButton.style.display = "none";
+	};
 
 	render() {
 		const liList = this.state.songs.map((song, index) => {
 			return (
-				<li key={index} onClick={() => this.change(index)}>
+				<li
+					key={index}
+					onClick={() => this.play(this.state.currentIndex)}>
 					<span> {index + 1}</span>
-					<span> {song.title} </span>
+					<span> {song.name} </span>
 				</li>
 			);
 		});
 
+		const audioPlayer = (
+			<>
+				<div>
+					<button
+						onClick={() => this.play(this.state.currentIndex - 1)}>
+						<i className="fa fa-caret-left" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.playButton = element)}
+						onClick={() => this.play(this.state.currentIndex)}>
+						<i className="fa fa-play" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.pauseButton = element)}
+						onClick={() => this.pause(this.state.currentIndex)}>
+						<i className="fa fa-pause" aria-hidden="true" />
+					</button>
+					<button
+						onClick={() => this.play(this.state.currentIndex + 1)}>
+						<i className="fa fa-caret-right" aria-hidden="true" />
+					</button>
+				</div>
+
+				<audio ref={element => (this.audio = element)} />
+			</>
+		);
+
 		return (
 			<>
-				<audio controls>
-					<source
-						src={this.state.songs[this.state.currentIndex].url}
-						type="audio/mp3"
-					/>
-				</audio>
-				<ul> {liList}</ul>
+				{audioPlayer}
+				<ul>{liList}</ul>
 			</>
 		);
 	}
