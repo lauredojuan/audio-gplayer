63217c2ecd4479d781d9224469a7d2678c194a18
Gerdeth <gerdethwickham@gmail.com>
2020-08-05 17:11:17 +0000

    player

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,21 +4,61 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			currentIndex: 1,
+			songs: [
+				{
+					title: "South Park",
+					id: "south-park",
+					author: "Kyle",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/south-park.mp3"
+				},
+				{
+					title: "Thunder Cats",
+					id: "thundercats",
+					author: "Moonra",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
+				},
+				{
+					title: "X-Men",
+					id: "x-men",
+					author: "Profesor",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
+				}
+			]
+		};
+	}
+
+	async change(i) {
+		await this.setState({ currentIndex: i });
+	}
+
+	render() {
+		const liList = this.state.songs.map((song, index) => {
+			return (
+				<li key={index} onClick={() => this.change(index)}>
+					<span> {index + 1}</span>
+					<span> {song.title} </span>
+				</li>
+			);
+		});
+
+		return (
+			<>
+				<audio controls>
+					<source
+						src={this.state.songs[this.state.currentIndex].url}
+						type="audio/mp3"
+					/>
+				</audio>
+				<ul> {liList}</ul>
+			</>
+		);
+	}
 }
